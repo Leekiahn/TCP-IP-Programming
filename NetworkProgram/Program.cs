b33edc4667212cbc8514f0b31c44eced7f7ef965
Leekiahn <68183471+Leekiahn@users.noreply.github.com>
2025-10-20 18:23:43 +0900

    test: 클라이언트 테스트

--- NetworkProgram/Program.cs
+++ NetworkProgram/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        TcpClientApp clientApp = new TcpClientApp();
+        clientApp.ClientRun();
     }
 }
